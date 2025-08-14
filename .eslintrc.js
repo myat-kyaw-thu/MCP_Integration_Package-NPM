4e569dcdb31900ce44ce53bef5e0e8d6c2541387
Andrew <myatkyawthu4002@gmail.com>
2025-08-14 21:20:41 +0630

    reform: the nacssary based files of proejct

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+export default {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    // Enforce consistent code style
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'no-console': 'off', // Allow console for CLI tool
+
+    // ES6+ specific rules
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'arrow-spacing': 'error',
+    'template-curly-spacing': 'error',
+
+    // Node.js specific
+    'no-process-exit': 'off', // Allow process.exit in CLI
+  },
+  globals: {
+    // Node.js globals
+    'process': 'readonly',
+    'Buffer': 'readonly',
+    '__dirname': 'readonly',
+    '__filename': 'readonly',
+  },
+};
