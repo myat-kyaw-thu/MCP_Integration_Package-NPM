58fe1aa1e5a6961e31623e0e858d8f03d6a1b6a5
Andrew <myatkyawthu4002@gmail.com>
2025-08-14 20:29:20 +0630

    removed: everything about buns

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Main entry point - exports public API
-export { defineMCP } from "./defineMCP.js";
-export { MCPConnectServer } from "./server/mcpServer.js";
-export type { MCPConfig, MCPTool, ToolDefinition } from "./types/mcp.js";
-
